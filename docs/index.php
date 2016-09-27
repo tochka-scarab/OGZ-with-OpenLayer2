c27aec727b80692280d9f9ac3bcd9efab7f86aee
tochka-scarab <tochka.scarab@gmail.com>
2016-09-28 00:20:31 +0300

    init

---
+++ docs/index.php
@@ -0,0 +1,35 @@
+﻿192.168.0.11	ECOPRO2			belenkov	
+192.168.0.108	STORE-MARZEK1		efanova
+192.168.0.136	VIPAL			sklad-marbel (Панченко)
+192.168.0.162	MPFACC01		borisov
+192.168.0.136	MARBEL-MANAGER		krivko
+192.168.0.118	MANAGER3		garazha
+//192.168.0.119	VIPAL			kurnosova
+192.168.0.121	MARBEL-MANAGER3		budchenko
+192.168.0.132	drawing6.rogan.local	zames
+192.168.0.124	MARBEL-ECO1		gniteev
+192.168.0.127	SALES5			personnel
+192.168.0.143	OFFICE			zubenko
+192.168.0.129	SECRETAR		priemnaya
+192.168.0.131	asus001-pc		pshenichnyj davidova
+192.168.0.132	MARBEL			psenichnaya
+192.168.0.1xx	MARBEL-MANAGER4		DopAdmin
+192.168.0.144	PROG02			kishynskaja
+192.168.0.145	ACCTH01.ROGAN.local	podgornaja
+192.168.0.152	MANAGER2		stahurskaya
+192.168.0.154	eco_marzek		Jury
+192.168.0.156	STATISTIC		paxomova
+192.168.0.161	DRAWING2		sereda
+192.168.1.11	ZARPLATA1		Belenkova_A	no
+192.168.0.140	TH-GB			zajceva
+192.168.1.121	MAINBUH			slabinskaya	no
+192.168.1.125	ACC02			antoshik
+192.168.0.158	STATISTIC		garaga
+192.168.0.139	45-URIST		exDubinetc
+
+https://login.1c.ru/login
+
+Логин: - 801004089
+Пароль: gzvAoPu1
+Логин: 800037323
+Пароль: GDv8qfV			
